619babe39a3210eff5b64550274db5496a699602
ludo <ludovic.jarno@axa.fr>
2024-06-07 14:57:10 +0200

    [Fromxxx] Attributes

--- Server/Controllers/UserRouteHandlers.cs
+++ Server/Controllers/UserRouteHandlers.cs
@@ -1,6 +1,7 @@
 using Server.Services.Interfaces;
 using Server.Models;
 using System.Runtime.CompilerServices;
+using Microsoft.AspNetCore.Mvc;
 
 [assembly: InternalsVisibleTo("Server.UnitTest")]
 namespace Server.Controllers;
@@ -9,15 +10,18 @@ public static class UserRouteHandlers
 {
     public static IEndpointRouteBuilder MapUserEndpoints(this IEndpointRouteBuilder builder)
     {
-        builder.MapGet("", GetListAsync);
-        builder.MapPost("", CreateAsync);
-        builder.MapGet("{id}", ReadAsync);
-        builder.MapPut("{id}", UpdateAsync);
-        builder.MapDelete("{id}", DeleteAsync);
+        builder.MapGet("/", GetListAsync);
+        builder.MapPost("/", CreateAsync);
+        builder.MapGet("/{id}", ReadAsync);
+        builder.MapPut("/{id}", UpdateAsync);
+        builder.MapDelete("/{id}", DeleteAsync);
         return builder;
     }
 
-    internal static async Task<IResult> GetListAsync(int? page, string search, IUserService userService)
+    internal static async Task<IResult> GetListAsync(
+        [FromQuery] int? page,
+        [FromQuery] string search,
+        [FromServices] IUserService userService)
     {
         DirectoryModel result = string.IsNullOrWhiteSpace(search)
             ? await userService.GetListAsync(page ?? 0)
@@ -25,25 +29,34 @@ public static class UserRouteHandlers
         return Results.Ok(result);
     }
 
-    internal static async Task<IResult> CreateAsync(UserModel input, IUserService userService)
+    internal static async Task<IResult> CreateAsync(
+        [FromBody] UserModel input,
+        [FromServices] IUserService userService)
     {
         UserModel result = await userService.CreateAsync(input);
         return Results.Created($"{result.Id}", result);
     }
 
-    internal static async Task<IResult> ReadAsync(string id, IUserService userService)
+    internal static async Task<IResult> ReadAsync(
+        [FromRoute] string id,
+        [FromServices] IUserService userService)
     {
         UserModel result = await userService.ReadAsync(id);
         return Results.Ok(result);
     }
 
-    internal static async Task<IResult> UpdateAsync(string id, UserModel input, IUserService userService)
+    internal static async Task<IResult> UpdateAsync(
+        [FromRoute] string id,
+        [FromBody] UserModel input,
+        [FromServices] IUserService userService)
     {
         UserModel result = await userService.UpdateAsync(id, input);
         return Results.Ok(result);
     }
 
-    internal static async Task<IResult> DeleteAsync(string id, IUserService userService)
+    internal static async Task<IResult> DeleteAsync(
+        [FromRoute] string id,
+        [FromServices] IUserService userService)
     {
         await userService.DeleteAsync(id);
         return Results.NoContent();
